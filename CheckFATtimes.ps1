50c66a2068ce7dfac1d27ed2dec9aff273dd1496
harriott <trohib@gmail.com>
2015-05-07 14:04:10 +0200

    formatted the output

--- CheckFATtimes.ps1
+++ CheckFATtimes.ps1
@@ -2,6 +2,7 @@
 
 # Joseph Harriott http://momentary.eu/ Last updated: Thu 07 May 2015
 
+# PowerShell 2.0
 # Check the time differences between matching files stored locally and on a FAT32 USB stick.
 # ------------------------------------------------------------------------------------------
 
@@ -18,12 +19,7 @@ $outf="CheckFATtimes-"+$computer+".txt"
 $now=Get-Date -format "ddd dd MMM yyyy HH:mm:ss"
 "$computer"+": $now" >> $outf
 "" >> $outf
-"Files on FAT32 drive's time offsets:" >> $outf
-"      3600   file's 1h newer than local" >> $outf
-"      7200   file's 2h newer than local" >> $outf
-"     -3600   file's 1h older than local" >> $outf
-"     -7200   file's 2h older than local" >> $outf
-"         -   file ain't there" >> $outf
+"Files on FAT32 drive's time offsets (-days.hours:minutes:seconds is older by that; - is not there):" >> $outf
 "" >> $outf
 
 gci $locald -r| # get all the directory contents recursively
@@ -31,6 +27,8 @@ foreach{
 	$localitem=$_.fullname
 	$liLWT=$_.LastWriteTime
 	$liTD=NEW-TIMESPAN -Start $now -End $liLWT
-	"$liTD $localitem" >> $outf
+	#"$liTD $localitem" >> $outf
+	"{0,14} {1,-1}" -f $liTD, $localitem >> $outf
 }
+Write-Output $results
 

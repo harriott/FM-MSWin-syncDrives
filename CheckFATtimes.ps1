c443f4ea55e585695cace79a4b631d4a009369b7
harriott <trohib@gmail.com>
2015-05-10 09:09:53 +0200

    added folder loop, and preparing intersection method

--- CheckFATtimes.ps1
+++ CheckFATtimes.ps1
@@ -10,7 +10,7 @@ $computer = gc env:computername
 $localrd="D:\Dropbox\"
 $lrdl=$localrd.length
 $FAT32rd="F:\"
-$locald=$localrd+"Current"
+$folders = @("Current\Git\mds-ToC\DailyLife", "Pointure_23\EsL\Le 6B FAR\c71 visit")
 
 # Prepare the output file:
 $outf=$FAT32rd+"CheckFATtimes-"+$computer+".txt"
@@ -28,23 +28,36 @@ $startts=Get-Date -format "ddd dd MMM yyyy HH:mm:ss"
 "         - file ain't there" >> $outf
 "" >> $outf
 
-echo "Getting $locald child-items"
-$localitems = gci $locald -r | where {$_.psIsContainer -eq $false} # get all the files recursively
-$totalCount = $localitems.Count
-echo "Comparing with "$FAT32rd"Current"
-$localitems | %{
-    $i = $i + 1
-    Write-Progress -Activity "Searching Files" -status "Searching File  $i of     $totalCount" -percentComplete ($i / $localitems.Count * 100)
-	$localfn=$_.fullname.substring(11)
-	$liLWT=$_.LastWriteTime
-	$FAT32fn=$FAT32rd+$localfn
-	if (test-path "$FAT32fn"){
-		$FAT32item=gci "$FAT32fn"
-		$F3LWT=$FAT32item.LastWriteTime
-		$F3SAhead=(NEW-TIMESPAN -Start $liLWT -End $F3LWT).totalseconds -replace '\.\d+$'
-	} else{
-		$F3SAhead="-"
+ForEach($folder in $folders){
+	$locald=$localrd+$folder
+	$FAT32d=$FAT32rd+$folder
+	# Get all the files recursively:
+	echo "Getting  $locald  child-items"
+	$localitems = gci $locald -r | where {$_.psIsContainer -eq $false}
+
+	# loop method:
+	$lm = Get-Date
+	$totalCount = $localitems.Count
+	$i = 0
+	$localitems | %{
+	    $i = $i + 1
+	    Write-Progress -Activity "Searching Files" -status "Searching File  $i of     $totalCount" -percentComplete ($i / $localitems.Count * 100)
+		$localfn=$_.fullname.substring(11)
+		$liLWT=$_.LastWriteTime
+		$FAT32fn=$FAT32rd+$localfn
+		if (test-path "$FAT32fn"){
+			$FAT32item=gci "$FAT32fn"
+			$F3LWT=$FAT32item.LastWriteTime
+			$F3SAhead=(NEW-TIMESPAN -Start $liLWT -End $F3LWT).totalseconds -replace '\.\d+$'
+		} else{
+			$F3SAhead="-"
+		}
+		if ($F3SAhead -ne "0"){ "{0,10} {1,-1}" -f $F3SAhead, $FAT32fn >> $outf }
 	}
-	if ($F3SAhead -ne "0"){ "{0,10} {1,-1}" -f $F3SAhead, $FAT32fn >> $outf }
+
+	# Intersection/difference method:
+	$idm = Get-Date
+	echo "Getting $FAT32d child-items"
+	$FAT32items = gci $FAT32d -r | where {$_.psIsContainer -eq $false}
 }
 

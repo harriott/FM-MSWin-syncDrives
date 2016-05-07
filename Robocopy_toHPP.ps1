87ab6f56b7c1695ef9ce5845c7372bebad4cc108
Joseph Harriott <Joseph Harriott>
2016-05-07 20:49:19 +0200

    after having lost half a day's Powershell notes

---
+++ Robocopy_toHPP.ps1
@@ -0,0 +1,12 @@
+# vim: set tw=0: http://momentary.eu/
+
+# Joseph Harriott - Fri 06 May 2016
+# Robocopy my personal file to HPP:11-n012na
+# PS C:\Users\Joseph> E:\Files\IT_stack\Robocopy_toHPP.ps1
+# ----------------------------------------------------------------------------------------------
+
+# "vim: nowrap tw=0:" > G:\Dr_Copied_toHPP.log; robocopy /mir G:\Dr_Copied E:\DropboxFiles\Copied /unilog+:G:\Dr_Copied_toHPP.log /tee /l
+# "vim: nowrap tw=0:" > G:\Dr_Photos_toHPP.log; robocopy /mir G:\Dr_Photos E:\DropboxFiles\Photos /unilog+:G:\Dr_Photos_toHPP.log /tee
+# "vim: nowrap tw=0:" > G:\Files_toHPP.log; robocopy /mir G:\Files E:\Files /unilog+:G:\Files_toHPP.log /tee
+"vim: nowrap tw=0:" > H:\Now_toHPP.log; robocopy /mir H:\Now E:\DropboxFiles\Now /unilog+:H:\Now_toHPP.log /tee
+

daa9ae145b7320c7f531022ce081c9dc5ddbe795
Joseph Harriott <trohib@gmail.com>
2016-05-10 20:50:56 +0200

    correct placement of tests for external folder presence

--- RobocopyHPP.ps1
+++ RobocopyHPP.ps1
@@ -9,15 +9,6 @@
 # G: Samsung M3
 # H: K16GB500
 
-""
-"Joseph, this Powershell script will use Robocopy to mirror your personal folders."
-[System.Console]::BackgroundColor = 'DarkCyan'
-[System.Console]::ForegroundColor = 'White'
-""
-$reply = Read-Host "Do you want to backup (b), or mirror TO (T) portable drives (or simulate (t)),  or mirror FROM (F) (or simulate (f))? "
-[System.Console]::ResetColor()
-""
-
 $backupFolder = "G:\Robocopy-backup-HPP"
 $FoldersArray = @(
   # first element of each row allows for that row to be switched off, by setting to 0
@@ -30,6 +21,16 @@ $FoldersArray = @(
   (0,"E:\Files","$backupFolder\Files","G:\Files"),
   (0,0,0) # dummy row
   )
+
+# Dialogue with myself:
+""
+"Joseph, this Powershell script will use Robocopy to mirror your personal folders."
+[System.Console]::BackgroundColor = 'DarkCyan'
+[System.Console]::ForegroundColor = 'White'
+""
+$reply = Read-Host "Do you want to backup (b), or mirror TO (T) portable drives (or simulate (t)),  or mirror FROM (F) (or simulate (f))? "
+[System.Console]::ResetColor()
+""
 
 if ($reply -ceq "b") {"Okay, running backups to $backupFolder`n"}
   elseif ($reply -ceq "T") {
@@ -50,8 +51,9 @@ if ($reply -ceq "b") {"Okay, running backups to $backupFolder`n"}
     ""
 	if ( $replyCheck -ne "y" ) {exit}
   } elseif ($reply -ceq "f") {"Okay, running simulation for `"mirror from external drives`"`n"}
+  else { exit }
 
-# Prepare a file to log of all of the changes made:
+# Prepare a file to log all of the changes made:
 $ChangesLog = $PSCommandPath.TrimEnd("ps1")+"log"
 $ThisScript = $PSCommandPath.TrimStart($PSScriptRoot)
 "vim: nowrap tw=0:" > $ChangesLog
@@ -59,13 +61,17 @@ $ThisScript = $PSCommandPath.TrimStart($PSScriptRoot)
 "Changes made by $ThisScript`:" >> $ChangesLog
 "" >> $ChangesLog
 
+# Attempt to do the work requested:
 foreach ($FolderControl in $FoldersArray) {
   if ( $FolderControl[0] ) {
     if ($reply -ceq "b") {
       $frFolder = $FolderControl[1]
       $toFolder = $FolderControl[2]
+      if ( ! $(Try { Test-Path $toFolder.trim() } Catch { $false }) ) {
+		  "Sorry, $toFolder  ain't there.`n"; continue }
 	} elseif ($reply -match "^t$|^f$") {
-
+      if ( ! $(Try { Test-Path $FolderControl[3].trim() } Catch { $false }) ) {
+		  "Sorry, "+$FolderControl[3]+"  ain't there.`n"; continue }
       $frFolder = $FolderControl[1]
       $toFolder = $FolderControl[3]
 	}
@@ -76,13 +82,13 @@ foreach ($FolderControl in $FoldersArray) {
 	""
     $Command0 = "`"vim: nowrap tw=0:`" > $LogFile"
     $Command1 = "robocopy /mir $frFolder $toFolder /np /unilog+:$LogFile /tee"
-    if ( $(Try { Test-Path $toFolder.trim() } Catch { $false }) ) {
-      # iex $Command0
-      # "" >> $LogFile
-      # "$Command0; $Command1" >> $LogFile
-      # iex $Command1
-    } else {$toFolder+" ain't there"}
+    # iex $Command0
+    # "" >> $LogFile
+    # "$Command0; $Command1" >> $LogFile
+    # iex $Command1
   }
 }
 ""
+
+# Sign off:
 Write-Host "All done and logged individually, with all of the changes saved together to  $ChangesLog" -background darkcyan -foreground white

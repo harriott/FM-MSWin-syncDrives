0aa76a9246b1d864e0cba92927a301c3da495908
Joseph Harriott <trohib@gmail.com>
2020-10-20 13:08:02 +0200

    silent!

--- RC-IdeaPad.ps1
+++ RC-IdeaPad.ps1
@@ -7,39 +7,55 @@
 
 # Prepare a file to log all of the changes made:
 $ThisScript = $PSCommandPath.TrimStart($PSScriptRoot)
-$ChangesLog = "D:\"+$ThisScript.TrimEnd("ps1")+"log"
-"vim: nowrap tw=0:" > $ChangesLog
+$Changes = "D:\"+$ThisScript.TrimEnd("ps1")
+$ChangesLog = $Changes+"log"
+$Changes = $Changes+"tmp"
+"vim: fdm=expr ft=RClog:" > $ChangesLog
 "" >> $ChangesLog
 
 $FoldersArray = @(
   "Sync0\Dr-JH-Cafezoide",
   "Sync0\Dr-JH-CforWork",
   "Sync0\Dr-JH-Copied",
+  "Sync0\Dr-JH-core",
   "Sync0\Dr-JH-F+F",
-  "Sync0\Dr-JH-IT_stack",
   "Sync0\Dr-JH-JCD-imagey-e3",
   "Sync0\Dr-JH-Now",
+  "Sync0\Dr-JH-Technos",
+  "Sync0\Dr-JH-Thechnos",
   "Sync0\Dr-JH-Theatre0",
   "Sync0\Dr-JH-Theatre1",
   "Sync0\Dr-JH-Then0",
   "Sync0\Dr-JH-Then1",
   "Sync0\Dr-JH-toReduce",
+  "Sync0\Dr-JH-TP.default-release",
   "Sync0\Dr-JH-Work",
   "Sync1\Dr-JH-Stack",
+  "Sync1\ThunderbirdProfiles",
   0) # dummy item
 
 # Do the work:
 foreach ($FE in $FoldersArray) {
   if ( $FE ) {
     $FD = $FE -replace '\\','-'
-    echo $FD
-    $Glog="D:\dir-$FD"+"_fromSM3.log"
-    "vim: nowrap tw=0:" > $Glog
-    "" >> $Glog
-    $Command = "robocopy /mir E:\$FE D:\$FD /np /unilog+:$Glog /tee /fft"
-    iex $Command # Comment this line to disable the file copying
-    $Command >> $ChangesLog
-    gc $Glog | select-string '    New File|    Newer|    Older|`*EXTRA File|  New Dir|`*EXTRA Dir' >> $ChangesLog
+    write-host $FD -foreground "magenta" -backgroundcolor "darkmagenta"
+    $folderLog = "D:\dir-$FD"+"_fromSM3.log"
+    # $simulate = ' /l'
+    $Command = "robocopy /mir E:\$FE D:\$FD /np /unilog+:$folderLog /tee /fft"+$simulate
+    # now (try) the changes
+      "vim: nowrap tw=0:" > $folderLog; "" >> $folderLog; iex $Command
+    "# $FD" >> $ChangesLog
+    "    $Command" >> $ChangesLog
+    $Changes = Get-Content $folderLog | select-string '    New File|    Newer|    Older|`*EXTRA File|  New Dir|`*EXTRA Dir'
+    if ( $Changes ) {
+      write-host "there were changes" -foreground "cyan" -backgroundcolor "darkmagenta"
+      ""
+      "" >> $ChangesLog
+      "Changes:" >> $ChangesLog
+      $Changes >> $ChangesLog }
     "" >> $ChangesLog
   }
 }
+
+gvim -c "silent! /^Changes:" $ChangesLog
+
